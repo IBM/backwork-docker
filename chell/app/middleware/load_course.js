f344f6db6031f92c22d3a3cc8c539ca7e26d6401
Henrique Zambon <henrique.zambon@ca.ibm.com>
2017-03-31 11:47:27 -0400

    Les is more (see what I did there?)

--- chell/app/middleware/load_course.js
+++ chell/app/middleware/load_course.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
-const debug = require('debug')('chell:middleware');
 
 function getCourseFilter(course) {
   return _.deeply(_.mapKeys)(course, (v, k) => _.snakeCase(k));
@@ -12,22 +11,13 @@ function loadCourse(req, res, next) {
   Course.findById(req.params.courseId).exec()
     .then((doc) => {
       if (!doc) {
-        const notFound = new Error('Not Found');
-        notFound.status = 404;
-        return next(notFound);
+        throw new Error(`Course with id: ${req.params.courseId} not found`);
       }
-
       const course = getCourseFilter(doc.toJSON());
       req.course = course; // eslint-disable-line no-param-reassign
       return next();
     })
-    .catch((err) => {
-      debug(err);
-
-      const notFound = new Error('Not Found');
-      notFound.status = 404;
-      next(notFound);
-    });
+    .catch(next);
 }
 
 module.exports = loadCourse;
