ca4558f41374398ab90c9df77457f1c52e068b52
Marcio Massaki Horoiwa <mhoroiwa@br.ibm.com>
2017-04-05 00:44:03 -0300

    Set precompile filters like Rails sprockets

--- chell/app/manifest.js
+++ chell/app/manifest.js
@@ -8,9 +8,17 @@ const environment = require('./mincer-environment');
 // Create and compile Manifest
 const manifest = new Mincer.Manifest(environment, path.join(__dirname, 'public', 'assets'));
 const isProduction = process.env.NODE_ENV === 'production';
+const files = [
+  (logicalPath, filename) => {
+    const ext = path.extname(logicalPath);
+    return /app\/assets/.test(filename) && !['.js', '.css'].includes(ext);
+  },
+  /^app\.(css|js)$/,
+  /\.(?:eot|svg|ttf|woff2?)$/, // fonts
+];
 
 try {
-  const assetsData = manifest.compile([/^[^_/][^/]*\.[^./]+$/], {
+  const assetsData = manifest.compile(files, {
     compress: true,
     sourceMaps: !isProduction,
     embedMappingComments: !isProduction,
