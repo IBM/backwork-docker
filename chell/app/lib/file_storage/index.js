f115ffad6850ffca1ab94b11f19f09c5c10e76da
Marcio Massaki Horoiwa <mhoroiwa@br.ibm.com>
2017-04-05 22:28:38 -0300

    Forward fileSotrage errors

--- chell/app/lib/file_storage/index.js
+++ chell/app/lib/file_storage/index.js
@@ -1,12 +1,5 @@
 const S3 = require('aws-sdk/clients/s3');
 
-function errorResponse(error) {
-  return {
-    code: error.code,
-    message: error.message,
-  };
-}
-
 function FileStorage(options) {
   this.s3 = new S3({
     endpoint: options.endpoint,
@@ -21,8 +14,7 @@ function FileStorage(options) {
 
 FileStorage.prototype.list = function () {
   return this.s3.listObjects().promise()
-    .then(results => results.Contents.map(item => item.Key))
-    .catch(errorResponse);
+    .then(results => results.Contents.map(item => item.Key));
 };
 
 // `name` is the full path to the file, relative to root
@@ -32,22 +24,19 @@ FileStorage.prototype.put = function (name, content) {
     Key: name,
     Body: content,
   }).promise()
-    .then(() => name) // { ETag: '"..."' }
-    .catch(errorResponse);
+    .then(() => name); // { ETag: '"..."' }
 };
 
 // `name` is the full path to the file, relative to root
 FileStorage.prototype.get = function (name) {
   return this.s3.getObject({ Key: name }).promise()
-    .then(result => result.Body)
-    .catch(errorResponse);
+    .then(result => result.Body);
 };
 
 // `name` is the full path to the file, relative to root
 FileStorage.prototype.delete = function (name) {
   return this.s3.deleteObject({ Key: name }).promise()
-    .then(() => name)
-    .catch(errorResponse);
+    .then(() => name);
 };
 
 module.exports = FileStorage;
