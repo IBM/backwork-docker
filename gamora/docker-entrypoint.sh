ff73f22d149e5b8c1bf99037a8652a55cefb89a4
Leons Petrazickis <leonsp@ca.ibm.com>
2017-06-11 14:14:10 -0600

    Mongo and MySQL backup. Softlayer upload broken

--- gamora/docker-entrypoint.sh
+++ gamora/docker-entrypoint.sh
@@ -1,24 +1,73 @@
 #!/bin/bash
 set -e
 
-FILENAME=mongo_backup_$(date +"%Y%m%d_%H%M%S").archive.gzip
+log() {
+  local message
+  message=$1
+  echo "[$(date)] $message" 1>&2
+}
+
+back_up_mongo() {
+  FILENAME=mongo_backup_$(date +"%Y%m%d_%H%M%S").archive.gzip
+
+  log "Taking mongo backup"
+  monsoon ${NOTIFICATION_SETTINGS} backup mongo \
+      -u "${MONGO_BACKUP_USER}" \
+      -p "${MONGO_BACKUP_PASSWORD}" \
+      --host="${MONGO_HOST}" \
+      --archive=/tmp/"${FILENAME}" \
+      --gzip
+  log "Done: Taking mongo backup"
+
+  log "Uploading mongo backup"
+  monsoon ${NOTIFICATION_SETTINGS} upload softlayer \
+      -u "${SOFTLAYER_USER}" \
+      -p "${SOFTLAYER_API_KEY}" \
+      -d "${SOFTLAYER_DATACENTER}" \
+      -c "${SOFTLAYER_CONTAINER}" \
+      /tmp/"${FILENAME}" \
+      mongo/"${FILENAME}"
+  log "Done: Uploading mongo backup"
+}
+
+back_up_mysql() {
+  FILENAME=mysql_backup_$(date +"%Y%m%d_%H%M%S").archive.gzip
+
+  log "Taking mysql backup"
+  set -x
+  monsoon ${NOTIFICATION_SETTINGS} backup mysql \
+      --output=/tmp/"${FILENAME}" \
+      --gzip \
+      --all-databases \
+      --single-transaction \
+      "--host=${MYSQL_HOST:?}" \
+      "--port=${MYSQL_PORT:?}" \
+      "--user=${MYSQL_USER:?}" \
+      "--password=${MYSQL_PASSWORD:?}"
+  set +x
+  log "Done: Taking mysql backup"
+
+  log "Uploading mysql backup"
+  monsoon ${NOTIFICATION_SETTINGS} upload softlayer \
+      -u "${SOFTLAYER_USER}" \
+      -p "${SOFTLAYER_API_KEY}" \
+      -d "${SOFTLAYER_DATACENTER}" \
+      -c "${SOFTLAYER_CONTAINER}" \
+      /tmp/"${FILENAME}" \
+      mongo/"${FILENAME}"
+  log "Done: Uploading mysql backup"
+}
+
+main() {
+  back_up_mongo
+  back_up_mysql
+}
+
+
 NOTIFICATION_SETTINGS=""
 
-if [[ $SENTRY_DSN ]]; then
+if [[ -n ${SENTRY_DSN:-} ]]; then
     NOTIFICATION_SETTINGS="-n sentry --sentry-dsn=${SENTRY_DSN}"
 fi
 
-monsoon ${NOTIFICATION_SETTINGS} backup mongo \
-    -u "${MONGO_BACKUP_USER}" \
-    -p "${MONGO_BACKUP_PASSWORD}" \
-    --host="${MONGO_HOST}" \
-    --archive=/tmp/"${FILENAME}" \
-    --gzip\
-&& \
-monsoon ${NOTIFICATION_SETTINGS} upload softlayer \
-    -u "${SOFTLAYER_USER}" \
-    -p "${SOFTLAYER_API_KEY}" \
-    -d "${SOFTLAYER_DATACENTER}" \
-    -c "${SOFTLAYER_CONTAINER}" \
-    /tmp/"${FILENAME}" \
-    mongo/"${FILENAME}"
+main
